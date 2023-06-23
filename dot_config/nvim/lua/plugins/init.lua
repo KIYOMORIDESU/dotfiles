a6fd138da3ebf39bea7b99f3c84ad9309467f3d1
KIYOMORIDESU <program.kiyomori@gmail.com>
2023-06-24 04:56:22 +0900

    init

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -227,6 +227,29 @@ local plugins = {
     end,
   },
 
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          -- default options: exact mode, multi window, all directions, with a backdrop
+          require("flash").jump()
+        end,
+      },
+      {
+        "S",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+      },
+    },
+  },
+
   -----------------------------------------------------------
   -- terminal
   -----------------------------------------------------------
