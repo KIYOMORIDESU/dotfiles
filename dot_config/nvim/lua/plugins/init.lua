c14f46e90d63ec48b1de95ad72c4dee7176ca472
shooooooooono <program.kiyomori@gmail.com>
2024-09-05 01:10:04 +0900

    add: copilot chat plugin

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -140,6 +140,23 @@ local plugins = {
     end
   },
 
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+    },
+    build = "make tiktoken",        -- Only on MacOS or Linux
+    opts = {
+      debug = true,                 -- Enable debugging
+      -- See Configuration section for rest
+    },
+    config = function() 
+      require("plugins.copilot-chat")
+    end
+  },
+
   -----------------------------------------------------------
   -- test runner
   -----------------------------------------------------------
