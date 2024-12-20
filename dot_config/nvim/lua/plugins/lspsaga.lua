d5981d6aa11bfa813ea64ecc9f17e17c6a52157d
shooooooooono <program.kiyomori@gmail.com>
2024-12-20 13:15:22 +0900

    update: lspsaga config

--- dot_config/nvim/lua/plugins/lspsaga.lua
+++ dot_config/nvim/lua/plugins/lspsaga.lua
@@ -4,5 +4,13 @@ saga.setup({
   border_style = "single",
   symbol_in_winbar = {
     enable = true,
+    sign = true,
+  },
+  implement = {
+    enable = true,
+    sign = true,
+  },
+  lightbulb = {
+    enabled = false,
   },
 })
