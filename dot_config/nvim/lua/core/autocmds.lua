21f3db2bb7ae3bc64df7bb51f968d059bd586406
shooooooooono <program.kiyomori@gmail.com>
2025-02-08 02:10:33 +0900

    [Neovim] InsertLeaveEventのときにfcitx5を英語に切り替える

--- dot_config/nvim/lua/core/autocmds.lua
+++ dot_config/nvim/lua/core/autocmds.lua
@@ -2,3 +2,14 @@
 -- Autocommand functions
 -----------------------------------------------------------
 vim.cmd("TransparentDisable")
+
+-- Normal mode になったときに、英語のIMEに切り替える
+if vim.loop.os_uname().sysname == "Linux" then
+  vim.api.nvim_create_autocmd("InsertLeave", {
+    callback = function()
+      if vim.fn.system("fcitx5-remote") == "2\n" then
+        vim.fn.system("fcitx5-remote -c")
+      end
+    end
+  })
+end
